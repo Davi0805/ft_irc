2cf3c600cb63733f8e2f56f3e7f4ebb5dfa55839
Davi0805 <melocardozo.video@gmail.com>
2025-03-11 21:45:51 +0000

    feat(PrivMsgCommand): If/else para mandar mensagem para canal ou user

--- Commands/PrivMsgCommand.cpp
+++ Commands/PrivMsgCommand.cpp
@@ -6,7 +6,7 @@
 /*   By: davi <davi@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 20:17:12 by davi              #+#    #+#             */
-/*   Updated: 2025/03/11 20:53:18 by davi             ###   ########.fr       */
+/*   Updated: 2025/03/11 21:25:01 by davi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,25 @@ void PrivMsgCommand::execute(MessageContent messageContent, int fd)
 
     // TODO: VERIFICACOES PARA EVITAR CRASHES
 
-    // TODO: OS CANAIS SAO CARACTERIZADOS COM UM # (JOGO DA VELHA NO INICIO DO NOME)
-    
-    if (_channelService->isUserPartOfChannel(fd, messageContent.tokens[1]))
+    // Se for um channel publico, comeca com # (jogo da velha no inicio)    
+    if (messageContent.tokens[1][0] == '#')
     {
-        std::vector<User*> users = _channelService->findChannel(messageContent.tokens[1])->getUsers();
-        for (size_t i = 0; i < users.size(); i++)
+        if (_channelService->isUserPartOfChannel(fd, messageContent.tokens[1]))
         {
-            if (users[i]->getFd() != fd)
-                send(users[i]->getFd(), messageContent.message.c_str(), messageContent.message.size(), 0);
+            std::vector<User*> users = _channelService->findChannel(messageContent.tokens[1])->getUsers();
+            for (size_t i = 0; i < users.size(); i++)
+            {
+                if (users[i]->getFd() != fd)
+                    send(users[i]->getFd(), messageContent.message.c_str(), messageContent.message.size(), 0);
+            }
         }
+    }
+    else
+    {
+        User* user = _userService->findUserByNickname(messageContent.tokens[1]);
+        if (user == NULL)
+            return ; // TODO: Exception
+        send(user->getFd(), messageContent.message.c_str(), messageContent.message.size(), 0);
     }
 }
 
