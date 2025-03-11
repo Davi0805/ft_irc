61313c6a7eb79ce050f647d0e196e78fbb7f4715
Davi Melo Cardozo <dmelo-ca@c2r5s3.42porto.com>
2025-03-11 18:02:46 +0000

    feat(Models/Channel): Add user method added

--- Models/Channel.cpp
+++ Models/Channel.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: davi <davi@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: dmelo-ca <dmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 01:09:26 by davi              #+#    #+#             */
-/*   Updated: 2025/03/11 01:16:23 by davi             ###   ########.fr       */
+/*   Updated: 2025/03/11 17:53:17 by dmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,9 @@ Channel::Channel(std::string ChannelName)
 
 Channel::~Channel()
 {
+}
+
+void Channel::AddUser(User* user)
+{
+    _users.push_back(user);
 }
