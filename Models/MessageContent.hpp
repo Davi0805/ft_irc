0680cb58c4dd9697a087c4a7f47ebf5d456c0444
Davi0805 <melocardozo.video@gmail.com>
2025-03-12 01:24:58 +0000

    doc(MessageContent): documentacao

--- Models/MessageContent.hpp
+++ Models/MessageContent.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   MessageContent.hpp                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dmelo-ca <dmelo-ca@student.42.fr>          +#+  +:+       +#+        */
+/*   By: davi <davi@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 13:16:58 by dmelo-ca          #+#    #+#             */
-/*   Updated: 2025/03/11 13:17:58 by dmelo-ca         ###   ########.fr       */
+/*   Updated: 2025/03/12 01:24:21 by davi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,15 @@
 #include <vector>
 #include <iostream>
 
+
+/* 
+    STRUCT COM OBJETIVO DE SIMPLIFICAR LOGICA DE TRANSMISSAO DE INFORMACOES,
+    ONDE NA PRATICA E UTILIZADA EM TODA A EXECUCAO DE COMANDOS, POSSUINDO UM
+    VETOR DE STRINGS COM TOKENS E MENSAGEM SEM TOKENIZACAO PARA PRESERVAR OS 
+    ESPACOS NATURAIS DA MENSAGEM
+
+    OBS: A MENSAGEM JA POSSUI ':'(DOIS PONTOS) DO RFC 
+*/
 struct MessageContent
 {
     std::vector<std::string> tokens;
