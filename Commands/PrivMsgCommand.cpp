df79537da8fb4e22e6604c47f064c74fc827eb26
Davi Melo Cardozo <dmelo-ca@c2r6s1.42porto.com>
2025-03-13 14:05:10 +0000

    feat(Commands/PrivMsgCommand): Handling DDC

--- Commands/PrivMsgCommand.cpp
+++ Commands/PrivMsgCommand.cpp
@@ -6,7 +6,7 @@
 /*   By: dmelo-ca <dmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 20:17:12 by davi              #+#    #+#             */
-/*   Updated: 2025/03/12 11:30:54 by dmelo-ca         ###   ########.fr       */
+/*   Updated: 2025/03/13 12:53:43 by dmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,15 +32,21 @@ void PrivMsgCommand::execute(MessageContent messageContent, int fd)
     std::cout << "[DEBUG]: COMANDO PRIVMSG SENDO CHAMADO" << std::endl;
 
     // TODO: VERIFICACOES PARA EVITAR CRASHES
+    User* sender = _userService->findUserByFd(fd);
 
-    // Se for um channel publico, comeca com # (jogo da velha no inicio)    
-    if (messageContent.tokens[1][0] == '#')
+    if (messageContent.tokens.size() == 2 && messageContent.message.find(":DCC") == 0)
+    {
+        std::string fullMsg = ServerMessages::ConvertMessageContentToA(messageContent);
+        User* receiver = _userService->findUserByNickname(messageContent.tokens[1]);
+
+        send(receiver->getFd(), fullMsg.c_str(), fullMsg.size(), 0);
+    }
+    else if (messageContent.tokens[1][0] == '#') // Se for um channel publico, comeca com # (jogo da velha no inicio)
     {
         if (_channelService->isUserPartOfChannel(fd, messageContent.tokens[1]))
         {
             std::vector<User*> users = _channelService->findChannel(messageContent.tokens[1])->getUsers();
             Channel* channel = _channelService->findChannel(messageContent.tokens[1]);
-            User* sender = _userService->findUserByFd(fd);
 
             std::string msgFormatada = ServerMessages::PrivMsgFormatter(sender, channel, messageContent.message);
 
@@ -53,10 +59,14 @@ void PrivMsgCommand::execute(MessageContent messageContent, int fd)
     }
     else
     {
-        User* user = _userService->findUserByNickname(messageContent.tokens[1]);
-        if (user == NULL)
+        User* receiver = _userService->findUserByNickname(messageContent.tokens[1]);
+        if (receiver == NULL)
             return ; // TODO: Exception
-        send(user->getFd(), messageContent.message.c_str(), messageContent.message.size(), 0);
+
+        std::string msgFormatada = ServerMessages::PrivMsgFormatter(sender, receiver, messageContent.message);
+        
+        //send(receiver->getFd(), messageContent.message.c_str(), messageContent.message.size(), 0);
+        send(receiver->getFd(), msgFormatada.c_str(), msgFormatada.size(), 0);
     }
 }
 
